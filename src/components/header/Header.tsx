4cc8081d59cdcdd34abc25e2bbb152300c091bcf
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-03-22 22:28:28 -0300

    Completed header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import { FiShoppingCart } from "react-icons/fi";
+
 export function Header() {
   return (
-    <div>
-      <h1 className="text-blue-600">Header</h1>
-    </div>
+    <header className="w-full px-1 bg-slate-200">
+      <nav className="w-full max-w-7xl h-14 flex  items-center justify-between px-5 mx-auto">
+        <Link className="font-bold text-2xl" to="/">
+          {" "}
+          Dev Shop
+        </Link>
+
+        <Link className=" relative" to="/cart">
+          <FiShoppingCart size={24} color="#121212" />
+          <span className=" absolute -top-3 left-3 flex justify-center items-center rounded-full w-5 h-5 bg-sky-500 text-white text-xs">
+            2
+          </span>
+        </Link>
+      </nav>
+    </header>
   );
 }
